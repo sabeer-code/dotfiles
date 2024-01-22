44d17f9fad8acba036135a3ffe6ceff8012a455a
Sabeer <sabeer.bakir+github@gmail.com>
2024-01-22 13:06:47 +0100

    Add Github copilot config

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -9,4 +9,22 @@ M.general = {
   }
 }
 
+M.copilot = {
+    i = {
+        ["<C-l>"] = {
+            function()
+                vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+            end,
+            "Copilot Accept",
+            {
+                replace_keycodes=true,
+                nowait=true,
+                silent=true,
+                expr=true,
+                noremap=true,
+            }
+        }
+    }
+}
+
 return M
