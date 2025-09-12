88f8140d78da0417da61c60de1850ed2567139f0
Sabeer <26003209+sabeer-code@users.noreply.github.com>
2025-09-12 16:48:18 +0100

    Change colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,9 @@
 local M = { -- Colorscheme settings
-	"rebelot/kanagawa.nvim",
+	"catppuccin/nvim",
+	name = "catppuccin",
 	priority = 1000, -- Make sure to load this before all the other start plugins.
 	init = function()
-		require("kanagawa").setup({
-			theme = "dragon", -- Load "wave" theme
-			background = { -- map the value of 'background' option to a theme
-				dark = "dragon",
-			},
-		})
-
-		vim.cmd.colorscheme("kanagawa")
-
-		-- You can configure highlights by doing something like:
-		vim.cmd.hi("Comment gui=none")
+		vim.cmd.colorscheme("catppuccin-frappe")
 	end,
 }
 
