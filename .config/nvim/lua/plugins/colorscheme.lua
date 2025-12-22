281365a12f7d48e6b43d14ee859a4e147c9a7a55
Sabeer <26003209+sabeer-code@users.noreply.github.com>
2025-12-22 17:21:07 +0000

    feat(nvim): Change colorscheme to catppuccin-mocha

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,6 +3,6 @@ return { -- Colorscheme settings
   name = "catppuccin",
   priority = 1000, -- Make sure to load this before all the other start plugins.
   init = function()
-    vim.cmd.colorscheme("catppuccin-frappe")
+    vim.cmd.colorscheme("catppuccin-mocha")
   end,
 }
