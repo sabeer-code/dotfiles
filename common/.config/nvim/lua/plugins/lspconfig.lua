a53b05e42d89d9471b9dc05b842dc18f1bea06c1
Sabeer <26003209+sabeer-code@users.noreply.github.com>
2026-08-31 13:08:55 +0100

    fix(nvim): Remove nvim lsp helper function

--- common/.config/nvim/lua/plugins/lspconfig.lua
+++ common/.config/nvim/lua/plugins/lspconfig.lua
@@ -5,26 +5,19 @@ vim.pack.add({
   h.gh("neovim/nvim-lspconfig"),
 })
 
--- Enabling LSPs
-local enable_lsp = function(lsp_name)
-  local lsp_cmd = vim.lsp.config[lsp_name].cmd[1]
 
-  if vim.fn.executable(lsp_cmd) == 1 then
-    vim.lsp.enable(lsp_name)
-  else
-    vim.notify(string.format("'%s' is not installed/executable.", lsp_cmd), vim.log.levels.WARN)
-  end
-end
-
-enable_lsp("lua_ls") -- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/lua_ls.lua
+vim.lsp.enable("lua_ls") -- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/lua_ls.lua
 
 -- Python
--- enable_lsp("pyright") -- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/pyright.lua
-enable_lsp("ty") -- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/ty.lua
-enable_lsp("ruff") -- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/ruff.lua
+-- vim.lsp.enable("pyright") -- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/pyright.lua
+vim.lsp.enable("ty") -- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/ty.lua
+vim.lsp.enable("ruff") -- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/ruff.lua
 
 -- Typescript/JavaScript
-enable_lsp("ts_ls") -- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/ts_ls.lua
+vim.lsp.enable("ts_ls") -- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/ts_ls.lua
 
 -- TOML
-enable_lsp("taplo") -- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/taplo.lua
+vim.lsp.enable("taplo") -- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/taplo.lua
+
+-- Java
+vim.lsp.enable("jdtls") -- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/jdtls.lua
