11f11f94014556026e1f5834f2b91f0ab1571e9e
Sabeer <26003209+sabeer-code@users.noreply.github.com>
2026-08-31 13:08:31 +0100

    chore(bash): Move bash aliases

---
+++ common/.bashrc.d/03-aliases.sh
@@ -0,0 +1,13 @@
+# Aliases
+alias ls="ls --color=auto"
+alias grep="grep --color=auto"
+alias fgrep="fgrep --color=auto"
+alias egrep="egrep --color=auto"
+
+# Other Command Line Aliases
+alias ll="ls -alF"
+alias la="ls -A"
+alias l="ls -CF"
+
+[[ -e "$(which nvim)" ]] && alias vim="nvim"
+
